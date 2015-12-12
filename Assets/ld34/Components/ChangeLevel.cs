0a5eaed6011afc36f6efba6a4051259cac46b7c9
Adam Wentz <adam@adamwentz.com>
2015-12-11 20:15:25 -0600

    Started unity project

---
+++ Assets/ld34/Components/ChangeLevel.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeLevel : MonoBehaviour {
+	public string levelName;
+
+	public void GotoLevel() {
+		Application.LoadLevel (levelName);
+	}
+}
