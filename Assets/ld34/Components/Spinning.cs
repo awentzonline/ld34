4b7cd47d30ba273d2db5d16757c97086275a99a9
Adam Wentz <adam@adamwentz.com>
2015-12-13 13:11:25 -0600

    spinning powerups

---
+++ Assets/ld34/Components/Spinning.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spinning : MonoBehaviour {
+	public Vector3 spinRate;
+
+	// Update is called once per frame
+	void Update () {
+		transform.localEulerAngles += spinRate * Time.deltaTime;
+	}
+}
